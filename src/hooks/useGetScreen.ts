f192656ff3c456f9f563503aa4994f095bee1d9b
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-07-16 20:33:06 +0100

    Update useGetScreen.ts

--- src/hooks/useGetScreen.ts
+++ src/hooks/useGetScreen.ts
@@ -1,5 +1,11 @@
 import { useRef } from "react";
 
 export default function useGetScreen() {
-  
+  const screenSize: number = useRef(window.innerWidth).current;
+  console.log(screenSize);
+  const screenType = {
+    isMobile: screenSize < 700,
+    isTablet: screenSize < 1000 && screenSize > 700,
+  };
+  return screenType;
 }
