486621dbfd6b9ddb449e66dd8caea73f6a758c87
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-07-17 10:58:04 +0100

    Update DocumentInput.tsx

--- src/ui/DocumentInput.tsx
+++ src/ui/DocumentInput.tsx
@@ -27,5 +27,38 @@ const StyledContainer = styled.div.withConfig({
     gap: ${({ islist }) => (islist ? "1rem" : "0.5rem")};
   }
 `;
+const StyledInputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
-export default DocumentInput;
+const StyledName = styled.span`
+  border: transparent;
+  background-color: transparent;
+  font-size: var(--font-size-medium);
+  color: var(--color-white-100);
+  padding: 5px 0px;
+  outline: none;
+  min-width: 15rem;
+  transition: border-bottom 0.2s ease;
+`;
+
+const StyledInput = styled.input<{ $ismobile?: boolean }>`
+  border: transparent;
+  background-color: transparent;
+  font-size: var(--font-size-medium);
+  color: var(--color-white-100);
+  padding: 5px 0px;
+  outline: none;
+  width: ${({ $ismobile }) => ($ismobile ? "10rem" : "15rem")};
+  transition: border-bottom 0.2s ease;
+  &::placeholder {
+    font-size: var(--font-size-medium);
+    color: var(--color-white-100);
+  }
+  &:hover,
+  &:focus {
+    border-bottom: solid var(--color-white-0) 1.5px;
+  }
+`;
+
