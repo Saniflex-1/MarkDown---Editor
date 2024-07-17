f3887ce5aa6d054cbe85cd42b3ab5121d9c540bf
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-07-17 10:52:00 +0100

    Update ContentBar.tsx

--- src/ui/ContentBar.tsx
+++ src/ui/ContentBar.tsx
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 
 const ContentBar = styled.div<{ $isDark?: boolean }>`
+  width: 100%;
+  height: 3rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
   
 `;
 export default ContentBar;
