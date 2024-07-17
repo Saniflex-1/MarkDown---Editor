f45857251bbf80c0c5df6d96f42be931f5c2f298
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-07-17 02:51:17 +0100

    Create Preview.tsx

---
+++ src/features/Preview.tsx
@@ -0,0 +1,7 @@
+import styled, { css } from "styled-components";
+import { parseMarkdown } from "../utils/helper";
+import ContentBar from "../ui/ContentBar";
+import useGetScreen from "../hooks/useGetScreen";
+import PreviewScreenIcons from "../ui/PreviewScreenIcons";
+import { useDarkModeContext } from "../Context/DarkModeContext";
+
