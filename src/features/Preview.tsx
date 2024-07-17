bc0b19b40a62a010a5a142f083777dcad02da0bd
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-07-17 02:53:03 +0100

    Update Preview.tsx

--- src/features/Preview.tsx
+++ src/features/Preview.tsx
@@ -5,3 +5,13 @@ import useGetScreen from "../hooks/useGetScreen";
 import PreviewScreenIcons from "../ui/PreviewScreenIcons";
 import { useDarkModeContext } from "../Context/DarkModeContext";
 
+const fullScreenStyles = css`
+  width: 100%;
+  display: flex;
+`;
+
+const EditorSection = styled.div`
+  width: 50%;
+  height: calc(100vh - 60px); /* Adjust height based on navbar height */
+`;
+
