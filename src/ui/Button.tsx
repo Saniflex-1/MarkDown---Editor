5cb83b2533d3d7bfe6d76abcdbe1d92c3ab22c89
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-07-17 10:31:02 +0100

    Create Button.tsx

---
+++ src/ui/Button.tsx
@@ -0,0 +1,15 @@
+import styled, { css } from "styled-components";
+
+interface ButtonProps {
+  large?: boolean;
+  $ismobile?: boolean;
+}
+
+const largeButton = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+
