e252f3f873ab0b51a6466a08a03924dccfa6eff3
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-07-17 11:34:00 +0100

    Create PreviewScreenIcons.tsx

---
+++ src/ui/PreviewScreenIcons.tsx
@@ -0,0 +1,14 @@
+import { FiEye } from "react-icons/fi";
+import { FiEyeOff } from "react-icons/fi";
+import styled, { css } from "styled-components";
+
+const IconStyle = css`
+  font-size: 20px;
+  color: var(--color-grey-100);
+  cursor: pointer;
+  &:hover {
+    color: var(--color-orange-0);
+  }
+`;
+
+
