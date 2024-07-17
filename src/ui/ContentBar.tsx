1743604b444bf491af3b5789db2c29038024f325
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-07-17 10:52:21 +0100

    Update ContentBar.tsx

--- src/ui/ContentBar.tsx
+++ src/ui/ContentBar.tsx
@@ -7,6 +7,12 @@ const ContentBar = styled.div<{ $isDark?: boolean }>`
   justify-content: space-between;
   align-items: center;
   padding: 0 20px;
-  
+  background-color: ${({ $isDark }) =>
+    $isDark ? "var(--color-dark-100)" : "var(--color-white-100)"};
+  color: ${({ $isDark }) =>
+    $isDark ? "var(--color-grey-200)" : "var(--color-grey-100)"};
+  font-size: var(--font-size-medium);
+  text-transform: uppercase;
+  letter-spacing: var(--char-spacing);
 `;
 export default ContentBar;
