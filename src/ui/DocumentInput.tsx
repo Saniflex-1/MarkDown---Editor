504b94f199fedaf1899a26f98ec4e7c114cf4818
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-07-17 10:57:19 +0100

    Update DocumentInput.tsx

--- src/ui/DocumentInput.tsx
+++ src/ui/DocumentInput.tsx
@@ -3,4 +3,29 @@ import Icon from "./Icon";
 import { ChangeEvent } from "react";
 import useGetScreen from "../hooks/useGetScreen";
 
+const listStyles = css`
+  cursor: pointer;
+`;
 
+const StyledContainer = styled.div.withConfig({
+  shouldForwardProp: (prop) => prop !== "islist",
+})<{ islist?: boolean }>`
+  ${({ islist }) => (islist ? listStyles : "")}
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  border-left: ${({ islist }) =>
+    islist ? "transparent" : "solid var(--color-grey-0) 1.5px"};
+  padding-left: ${({ islist }) => (islist ? "0" : "10px")};
+  transition: color 300ms;
+  &:hover .documentName {
+    color: var(--color-orange-0);
+  }
+  @media screen and (max-width: 700px) {
+    border-left: transparent;
+    padding-left: 0;
+    gap: ${({ islist }) => (islist ? "1rem" : "0.5rem")};
+  }
+`;
+
+export default DocumentInput;
