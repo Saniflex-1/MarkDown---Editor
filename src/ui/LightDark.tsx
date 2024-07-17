e00ce23a303c36dd0d144746cb1d42fbcd179437
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-07-17 11:30:08 +0100

    Create LightDark.tsx

---
+++ src/ui/LightDark.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { LuSunMedium } from "react-icons/lu";
+import { IoMoonOutline } from "react-icons/io5";
+import { useDarkModeContext } from "../Context/DarkModeContext";
+
+const ThemeContainer = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 5px;
+  align-items: center;
+`;
+
+
