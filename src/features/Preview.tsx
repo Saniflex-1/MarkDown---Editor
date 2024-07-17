aaf82457a16542ad19c2c4774b2265d7b3ee8ee5
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-07-17 02:55:38 +0100

    Update Preview.tsx

--- src/features/Preview.tsx
+++ src/features/Preview.tsx
@@ -15,3 +15,74 @@ const EditorSection = styled.div`
   height: calc(100vh - 60px); /* Adjust height based on navbar height */
 `;
 
+const PreviewSection = styled(EditorSection).withConfig({
+  shouldForwardProp: (prop) => prop !== "isfullscreen",
+})<{ isfullscreen: boolean; $isDark?: boolean }>`
+  align-items: center;
+  flex-direction: column;
+
+  /* state styles */
+  /*border color change  */
+  border-left: 1px solid
+    ${({ $isDark }) =>
+      $isDark ? "var(--color-grey-0)" : "var(--color-grey-300)"};
+
+  /* style change based on whether the preview is fullscreen */
+  ${({ isfullscreen }) => (isfullscreen ? fullScreenStyles : "")}
+  /* background color change */
+  background-color: ${({ $isDark }) =>
+    $isDark ? "var(--color-dark-0)" : "var(--color-white-0)"};
+`;
+
+const PreviewWrapper = styled.div.withConfig({
+  shouldForwardProp: (prop) => prop !== "isfullscreen",
+})<{ isfullscreen: boolean; $isDark: boolean }>`
+  display: flex;
+  background-color: ${({ $isDark }) =>
+    $isDark ? "var(--color-white-d-0)" : "var(--color-white-0)"};
+  flex-direction: column;
+  overflow-y: auto;
+  align-items: center;
+  width: ${({ isfullscreen }) => (isfullscreen ? "50%" : "100%")};
+  height: 100%;
+  padding-bottom: 20px;
+  @media screen and (max-width: 1000px) {
+    width: 100%;
+  }
+`;
+
+const StyledPreview = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+interface PreviewProps {
+  input: string;
+  fullPreview: boolean;
+  setFullPreview: (parameter: boolean) => void;
+}
+export default function Preview({
+  input,
+  fullPreview,
+  setFullPreview,
+}: PreviewProps) {
+  const { isMobile } = useGetScreen();
+  const { isDarkMode } = useDarkModeContext();
+  return isMobile && !fullPreview ? (
+    ""
+  ) : (
+    <PreviewSection $isDark={isDarkMode} isfullscreen={fullPreview}>
+      <ContentBar $isDark={isDarkMode}>
+        <span>preview</span>
+        <PreviewScreenIcons state={fullPreview} setState={setFullPreview} />
+      </ContentBar>
+      <PreviewWrapper $isDark={isDarkMode} isfullscreen={fullPreview}>
+        <StyledPreview>{parseMarkdown(input, isDarkMode)}</StyledPreview>
+      </PreviewWrapper>
+    </PreviewSection>
+  );
+}
