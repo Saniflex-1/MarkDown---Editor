20401d569ebb677a5f4c89c83bdfa09d9d1cc0a7
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-07-17 10:32:16 +0100

    Update Button.tsx

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -12,6 +12,29 @@ const largeButton = css`
   width: 100%;
 `;
 
+const Button = styled.button.withConfig({
+  shouldForwardProp: (prop) => prop !== "large",
+})<ButtonProps>`
+  background-color: var(--color-orange-0);
+  padding: ${({ large }) => (large ? "10px 20px" : "10px")};
+  transition: background-color 0.3s ease;
+  font-size: var(--font-size-medium);
+  color: var(--color-white-0);
+  text-transform: capitalize;
 
+  border-radius: 3px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.3rem;
+  &:hover {
+    background-color: var(--color-orange-100);
+  }
+  ${({ large }) => (large ? largeButton : "")}
+
+  @media screen and (max-width: 700px) {
+    padding: ${({ $ismobile }) => ($ismobile ? "10px" : "10px 20px")};
+  }
+`;
 
 export default Button;
