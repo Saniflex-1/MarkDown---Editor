fa785a0f45fb000470dc2d56176742ce286c1585
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-07-17 16:21:40 +0100

    Update Paragraph.tsx
    A component for a normal paragraph

--- src/ui/Paragraph.tsx
+++ src/ui/Paragraph.tsx
@@ -6,3 +6,36 @@ interface ParagraphType {
   $isDark?: boolean;
 }
 
+const paragraphStyles = {
+  normal: css``,
+  numberPoint: css`
+    padding-left: 20px;
+    display: flex;
+    gap: 10px;
+  `,
+  point: css`
+    padding-left: 20px;
+    margin-left: 20px;
+    &::before {
+      content: "•";
+      color: var(--color-orange-0);
+      position: absolute;
+      left: 0;
+      top: 3px;
+    }
+  `,
+};
+
+const Paragraph = styled.p<ParagraphType>`
+  ${({ type = "normal" }) => paragraphStyles[type]}
+  position: relative;
+  color: ${({ $isDark }) =>
+    $isDark ? "var(--color-grey-200)" : "var(--color-grey-100)"};
+  line-height: var(--line-height);
+  font-family: var(--roboto-slap);
+  font-size: var(--font-size-small);
+
+  word-wrap: break-word;
+`;
+
+export default Paragraph;
